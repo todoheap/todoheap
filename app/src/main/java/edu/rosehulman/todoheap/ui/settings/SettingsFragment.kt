dcc9221907c8e02b19a97a9d3852d7c127e871bc
Michael Zhao <zhaoy12@rose-hulman.edu>
2021-01-18 14:02:10 -0500

    settings page

---
+++ app/src/main/java/edu/rosehulman/todoheap/ui/settings/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package edu.rosehulman.todoheap.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import edu.rosehulman.todoheap.R
+import edu.rosehulman.todoheap.ui.notifications.NotificationsViewModel
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_notifications)
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return root
+    }
+}
