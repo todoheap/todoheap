20986aab45acbfc821a7c4c4d67c0d16fe96805d
Jonathan <moyersjm@rose-hulman.edu>
2021-02-11 19:46:07 -0500

    deleted breaking thing

--- app/src/main/java/edu/rosehulman/todoheap/main/FirebaseNotificationService.kt
+++
@@ -1,9 +0,0 @@
-package edu.rosehulman.todoheap.main
-
-import com.google.firebase.messaging.FirebaseMessagingService
-
-class FirebaseNotificationService:FirebaseMessagingService() {
-    override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
-    }
-}
